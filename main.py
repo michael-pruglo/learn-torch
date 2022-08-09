69bc453a435f658a5b14d50228b3dded26c453ec
m.pruglo <m.pruglo@cleack.com>
2022-08-09 18:03:11 +0300

    MNIST data loaders

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-import torch
+import torch, torchvision, torchvision.transforms as transforms
+import numpy as np, matplotlib.pyplot as plt
 
+
+def get_data(training):
+    return torchvision.datasets.MNIST(
+        root="./datasets/",
+        train=True,
+        download=True,
+        transform=transforms.ToTensor()
+    )
 
 def main():
-    t = torch.rand(6, 4)
-    print(t.dtype)
-    print(t, t.shape)
-    print(t.device)
-    print(t.layout)
-    t = t.reshape(8, 3)
-    print(t, t.shape)
+    training_set_loader =   torch.utils.data.DataLoader(get_data(training=True), batch_size=10)
+    validation_set_loader = torch.utils.data.DataLoader(get_data(training=False), batch_size=10)
 
-    print("torch version", torch.__version__)
-    print(torch.cuda.is_available())
-    print(torch.version.cuda)
+    training_batch = next(iter(training_set_loader))
+    images, labels = training_batch
+    
+    print(images.squeeze().shape)
+    print("labels:", labels)
+    grid = torchvision.utils.make_grid(images)
+    plt.figure(figsize=(15,15))
+    plt.imshow(np.transpose(grid, (1,2,0)))
+    plt.show()
 
 
 if __name__ == "__main__":
